fbab7b35c0153fb6fb174fa7990cd596b807ba98
Weiliang Hu <weilianghu@Weiliangs-MacBook-Pro.local>
2021-09-16 15:01:07 -0400

    added a py file

---
+++ 9:16Excercise.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+mortality = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+a = mortality[
+    [
+        "Mortality rate, infant (per 1,000 live births)",
+        "GDP per capita (constant 2010 US$)",
+        "Country Name",
+    ]
+]
+
+plt.scatter(
+    a["GDP per capita (constant 2010 US$)"],
+    a["Mortality rate, infant (per 1,000 live births)"],
+    alpha=0.5,
+)
+plt.show()
